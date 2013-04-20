a13bad4928384316e06d06f626f920e6b817ce8e
Leonard Chioveanu <leonard.chioveanu@gmail.com>
2013-04-20 17:46:45 +0300

    Add addon_config makefile. Keyboard and mouse inputs to LuaApp.

--- src/ofLuaApp.cpp
+++ src/ofLuaApp.cpp
@@ -31,3 +31,68 @@ void ofLuaApp::exit() {
     if (!dub_pushcallback("exit")) return;
     dub_call(1, 0);
 }
+
+//--------------------------------------------------------------
+void ofLuaApp::keyPressed(int key) {
+    if (!dub_pushcallback("keyPressed")) return;
+    
+    lua_pushnumber(dub_L, key);
+    
+    dub_call(2, 0);
+}
+
+void ofLuaApp::keyReleased(int key) {
+}
+
+//--------------------------------------------------------------
+void ofLuaApp::mouseMoved(int x, int y) {
+    if (!dub_pushcallback("mouseMoved")) return;
+    
+    lua_pushnumber(dub_L, x);
+    lua_pushnumber(dub_L, y);
+    
+    dub_call(3, 0);
+}
+
+//--------------------------------------------------------------
+void ofLuaApp::mouseDragged(int x, int y, int button) {
+    if (!dub_pushcallback("mouseDragged")) return;
+    
+    lua_pushnumber(dub_L, x);
+    lua_pushnumber(dub_L, y);
+    lua_pushnumber(dub_L, button);
+    
+    dub_call(4, 0);
+}
+
+//--------------------------------------------------------------
+void ofLuaApp::mousePressed(int x, int y, int button) {
+    if (!dub_pushcallback("mousePressed")) return;
+    
+    lua_pushnumber(dub_L, x);
+    lua_pushnumber(dub_L, y);
+    lua_pushnumber(dub_L, button);
+    
+    dub_call(4, 0);
+}
+
+//--------------------------------------------------------------
+void ofLuaApp::mouseReleased(int x, int y, int button) {
+    if (!dub_pushcallback("mouseReleased")) return;
+    
+    lua_pushnumber(dub_L, x);
+    lua_pushnumber(dub_L, y);
+    lua_pushnumber(dub_L, button);
+    
+    dub_call(4, 0);
+}
+
+//--------------------------------------------------------------
+void ofLuaApp::windowResized(int w, int h) {
+    if (!dub_pushcallback("windowResized")) return;
+    
+    lua_pushnumber(dub_L, w);
+    lua_pushnumber(dub_L, h);
+    
+    dub_call(3, 0);
+}
