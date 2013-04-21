263bf8d9fa113c6ebf3758e6ea708fb5c17c9da3
Bogdan Florescu <adonissoft@gmail.com>
2013-04-21 05:19:40 +0300

    fix particles demo

--- examples/particles/app/ParticleSystem.lua
+++ examples/particles/app/ParticleSystem.lua
@@ -3,34 +3,46 @@ class 'ParticleSystem'
 function ParticleSystem:ParticleSystem(imgs, v)
     self.textures = imgs
     self.particles = {}
+
 end
 
 function ParticleSystem:run()
         for i = table.getn(self.particles),1,-1 do
-            local p = particles[i];
+            local p = self.particles[i];
             p:run()
-            if p:isDead() then
+            if p:isDead() == true then
                 table.remove(self.particles,i);
             end
         end
 end
     
 function ParticleSystem: addParticle( x,  y) 
-    r = ofRandom(table.getn(textures));
-    table.insert(particles,Particle(x,y,textures[r]));
+
+    r = math.floor(ofRandom(table.getn(self.textures)));
+    
+    if r==0 then
+        r = 1;
+    elseif r > table.getn(self.textures) then
+        r = table.getn(self.textures)
+    end
+    table.insert(self.particles,Particle(x,y,self.textures[r]));
 end
 
     
 function ParticleSystem:applyForces(f)
-    for _,p in ipairs(self.particles) do
-        p:applyForce(f);
+
+    for i=1,table.getn(self.particles) do
+        self.particles[i]:applyForce(f);
     end
 end
     
+--[[
 function ParticleSystem: addParticle(p)
+    print("insert",p)
         table.insert(self.particles,p)
 end
-    
+]]--
+
 function ParticleSystem: dead()
     if table.getn(self.particles) == 0 then
         return true;
